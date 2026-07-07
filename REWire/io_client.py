74867af8ca779e6a60bea5716c10fe94a95da37b
omidbimo <kompani.omid@gmail.com>
2026-07-07 15:49:02 +0200

    Add async send/receive to the IO client.

--- REWire/io_client.py
+++ REWire/io_client.py
@@ -1,8 +1,13 @@
-from time import sleep
+
 from typing import Any, Optional, Tuple, Union
+import time
 import inspect
 import random
 import logging
+import socket
+import selectors
+import threading
+
 logger = logging.getLogger(__name__)
 
 from . import eip_encapsulation as eip_encap
@@ -40,18 +45,11 @@ from .common import (
 from .objects.cip_object import CIPObjectFactory as CIPObject
 from .exceptions import CIPError
 from .unconnected_client import UnconnectedClient
-
+from .connected_client import ConnectionTriad
 
 __all__ = [
         "IOClient",
         ]
-
-
-class Class3PDU(Packet):
-    _fields = (
-        ("sequence_count", UINT(0)),
-        ("payload", BYTES()),
-        )
 
 
 class Class0_1_Packet(Packet):
@@ -63,9 +61,12 @@ class Class0_1_Packet(Packet):
 
 
 class IOClient:
+    _io_scheduler = None
+
     def __init__(self,
             unconnected_client,
             connection_path: PaddedEPATH, # b"\x04\x20\x04\x24\x01\x2C\x64\x2C\x65"
+            transport_class=1,
             o2t_rpi = 1000000, # uSec resolution 1000x4 -> 4 sec
             t2o_rpi = 1000000, # uSec resolution 1000x4 -> 4 sec
             timeout_multiplier = 0, # 4x RPI: connection timeout
@@ -73,7 +74,7 @@ class IOClient:
             t2o_size = 100,
             o2t_prio = 2,
             t2o_prio = 2,
-            vendor_id = 0,
+            vendor_id = 65530,
             originator_serial_number = 0xC0FFEE,
             ekey: ELECTRONIC_KEY_SEGMENT = None,
             ucmm_timeout = 5000, # milliseconds
@@ -84,6 +85,9 @@ class IOClient:
             self.connection_path = PaddedEPATH.unpack(connection_path)
         else:
             self.connection_path = connection_path
+
+        if transport_class > 1:
+            raise Exception(f"Unsupported transport class: {transport_class} for an implicit connection.")
 
         self.o2t_rpi = o2t_rpi
         self.t2o_rpi = t2o_rpi
@@ -96,12 +100,99 @@ class IOClient:
         self.connection_serial_number = 0
         self.ucmm_timeout = ucmm_timeout
         self.isconnected = False
-        self.seq_counter = UINT(0)
+        self.transport_class = transport_class
         self.session = None
+
+        self._peer_ip = None
+        self._o2t_socket = None
+        self._t2o_socket = None
+        self._next_send = None
+        self._producing_encap_seq_counter = UDINT(0)
+        self._producing_data_sequence_count = UINT(0)
+        self._producing_run_idle = UDINT(0)
+        self._producer_data_pending = bytearray(bytes(t2o_size))
+        self._producer_data = bytearray()
+
+        self._consuming_encap_seq_counter = UDINT(0)
+        self._consuming_data_sequence_count = UINT(0)
+        self._consuming_run_idle = UDINT(0)
+        self._consumer_data_pending = bytearray()
+        self._consumer_data = bytearray()
+
+
+        if self._io_scheduler is None:
+            self._io_scheduler = IOScheduler()
+
+    @property
+    def rpi(self):
+        return self.o2t_api
+
+    @property
+    def socket(self):
+        return self._o2t_socket
+
+    @property
+    def next_send(self):
+        return self._next_send
+
+    @property
+    def received_data(self):
+        self._consumer_data[:] = self._consumer_data_pending
+        return self._consumer_data
+
+    def update_output(self, data:bytes, data_sequence_counter=0, run_idle_value=None):
+        data_counter = UINT(data_sequence_counter & 0xFFFF)
+        run_idle_header = BYTES()
+        if run_idle_value is not None:
+            run_idle_header = UDINT(run_idle_value & 0xFFFFFFFF)
+
+        self._producer_data_pending = bytearray(data_counter.pack() + run_idle_header.pack() + data)
+
+    def _send(self):
+        self._producer_data[:] = self._producer_data_pending
+        self._producing_encap_seq_counter += 1
+        o2t_packet = Class0_1_Packet()
+        o2t_packet.address_item= SequencedAddressItem(connection_identifier=self.cip_producer_connection_id,
+                                                      encapsulation_sequence_number=self._producing_encap_seq_counter)
+        o2t_packet.data_item = ConnectedDataItem(length=len(self._producer_data), data=self._producer_data)
+
+        self._o2t_socket.sendto(o2t_packet.pack(), (self._peer_ip, 2222))
+
+    def _receive(self):
+        data, addr = self.socket.recvfrom(1500)
+
+        t2o_packet = Class0_1_Packet.unpack(data)
+
+        if t2o_packet.address_item.connection_identifier != self.cip_consumer_connection_id:
+            raise Exception("Unexpected O->T Connection Id! Expected: " \
+                f"0x{self.cip_consumer_connection_id:08X}, got: 0x{t2o_packet.address_item.connection_identifier:08X}")
+
+        if t2o_packet.address_item.encapsulation_sequence_number != self._consuming_encap_seq_counter + 1:
+            raise Exception("Unexpected O->T sequence number! Expected: "
+                f"0x{self._consuming_encap_seq_counter:08X}" \
+                f", got: 0x{t2o_packet.address_item.encapsulation_sequence_number:08X}")
+
+        self._consuming_encap_seq_counter = t2o_packet.address_item.encapsulation_sequence_number
+        self._consumer_data_pending = t2o_packet.data_item.data[:t2o_packet.data_item.length]
+        self.isconnected = True
+
+    def is_send_due(self, now):
+        return now >= self._next_send
+
+    def schedule_next_send(self, now):
+        while self._next_send <= now:
+            self._next_send += (self.o2t_api / 1000000.0)
+
+    def start(self):
+        self._next_send = time.monotonic()
+        self._io_scheduler.add(self)
+        self._io_scheduler.start()
+        logger.info("Starting Class 1 producer/consumer - " \
+            f"<O->T: 0x{self.cip_producer_connection_id:08X}, T->O: 0x{self.cip_consumer_connection_id:08X}>")
 
     def open(self):
         transport_class_and_trigger = TransportClassTrigger(
-                TransportClass.CLASS1,
+                TransportClass.CLASS0 if self.transport_class==0 else TransportClass.CLASS1 ,
                 ProductionTrigger.CYCLIC,
                 TransportDirection.CLIENT
                 )
@@ -188,14 +279,18 @@ class IOClient:
                         connection_path = self.connection_path,
                         )
         # Unpacking the response
-        o2t_api, t2o_api, connection_serial_number, o2t_network_connection_id, t2o_network_connection_id = rsp
+        o2t_api, t2o_api, connection_serial_number, o2t_network_connection_id, t2o_network_connection_id, to2_port , t2o_addr = rsp
 
+        self.o2t_api = o2t_api
+        self.t2o_api = t2o_api
         self.connection_serial_number = connection_serial_number
         self.cip_producer_connection_id = o2t_network_connection_id
         self.cip_consumer_connection_id = t2o_network_connection_id
         self.session.add_owner(self)
-        self.isconnected = True
-        logger.debug("Class 3 connection id:0x{:X} is established.".format(o2t_network_connection_id))
+        self._o2t_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self._o2t_socket.bind((socket.inet_ntoa(t2o_addr.to_bytes(4, "big")), 2222))
+        self._peer_ip = self.session.peer_ip
+        logger.info(f"Class 1 connection{self.connection_triad} is opened.")
 
     def close(self):
         """
@@ -219,8 +314,7 @@ class IOClient:
                          self.originator_vendor_id,
                          self.originator_serial_number)
 
-        logger.debug("Class 3 connection id:0x{:08X} is closed.".format(
-                self.cip_producer_connection_id))
+        logger.info(f"Class 1 connection{self.connection_triad} is closed.")
 
         self.cip_producer_connection_id = None
         self.cip_consumer_connection_id = None
@@ -228,8 +322,87 @@ class IOClient:
         self.session = None
         self.ucc = None
 
+
+
     @property
-    def triad(self):
-        return (self.connection_serial_number,
-                self.originator_vendor_id,
-                self.originator_serial_number)
+    def connection_triad(self):
+        return ConnectionTriad(
+                    self.connection_serial_number,
+                    self.originator_serial_number,
+                    self.originator_vendor_id,
+                    )
+class IOScheduler:
+    """Schedules and dispatches multiple EtherNet/IP Class 1 connections."""
+
+    def __init__(self):
+        self._selector = selectors.DefaultSelector()
+        self._connections = {}
+        self._lock = threading.Lock()
+
+        self._running = False
+        self._thread = None
+
+    def add(self, connection):
+        """
+        Register a Class1Connection.
+
+        The connection must provide:
+
+            _receive()
+            _send()
+            next_send
+        """
+        with self._lock:
+            self._connections[connection.connection_triad] = connection
+            self._selector.register(connection.socket, selectors.EVENT_READ, connection)
+
+    def remove(self, connection):
+        with self._lock:
+            self._connections.pop(connection.connection_triad, None)
+
+            try:
+                self._selector.unregister(connection.socket)
+            except Exception:
+                pass
+
+    def start(self):
+        if self._running:
+            return
+
+        self._running = True
+        self._thread = threading.Thread(target=self._run, daemon=True, name="Scheduler")
+        self._thread.start()
+
+    def stop(self):
+        self._running = False
+
+        if self._thread:
+            self._thread.join()
+
+    def _run(self):
+        while self._running:
+
+            with self._lock:
+                connections = list(self._connections.values())
+
+            if not connections:
+                time.sleep(0.1)
+                continue
+
+            # Calculating the next send/receive deadline
+            now = time.monotonic()
+            next_deadline = min(conn.next_send for conn in connections)
+            timeout = max(0.0, next_deadline - now)
+
+            receive_events = self._selector.select(timeout)
+
+            for event, _ in receive_events:
+                connection = event.data
+                connection._receive()
+
+            now = time.monotonic()
+
+            for connection in connections:
+                if connection.is_send_due(now):
+                    connection._send()
+                    connection.schedule_next_send(now)
